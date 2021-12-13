e6230cdb404561d6bb3112a73faa86010fa22c87
Heather Violet <heather@heathers-air.lan>
2021-12-12 17:59:15 -0800

    Js complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,14 @@
-var saveBtn = $("saveBtn");
-
-// current day at top
-var currentDay = moment().format("MMM Do YY");
-$("#currentDay").append(currentDay);
-
-
-
-//save button click
-   saveBtn.on("click", function () {
-    //console.log(this);
+    // moment.js    
+    $("#currentDay").text(moment().format("MMM Do YY"));    
     
-    var text = $(this).siblings(".hour").val();
-    var time = $(this).parent().attr("id"); 
-
-    //set items in local storage.
-    localStorage.setItem(time, text);
-})
-
+    // event listener for saveBtn
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+        //stores values in local storage
+        localStorage.setItem(time, text);
+    })
+    
 // retrieves from local storage
 $("#8AM .description").val(localStorage.getItem("8AM"));
 $("#9AM .description").val(localStorage.getItem("9AM"));
@@ -28,3 +20,32 @@ $("#2PM .description").val(localStorage.getItem("2PM"));
 $("#3PM .description").val(localStorage.getItem("3PM"));
 $("#4PM .description").val(localStorage.getItem("4PM"));
 $("#5PM .description").val(localStorage.getItem("5PM"));
+
+function currentHour() {
+    //get current hour
+    var currentHour = moment().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+       
+
+        //checks time and colorcodes based of current time
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+currentHour(); 
